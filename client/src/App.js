6cee4e8ce7c35081f77271bf98c51f2d803f09e6
ericbanker12 <ericbanker12@gmail.com>
2019-09-16 15:57:56 -0500

    add saved list function

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,14 @@ const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    if (!savedList.map(e=>e.id).includes(movie.id)) setSavedList( [...savedList, movie] );
   };
 
   return (
     <div>
       <SavedList list={savedList} />
       <Route exact path='/' component={MovieList} />
-      <Route path='/movies/:id' component={Movie} />
+      <Route path='/movies/:id' render={(props) => <Movie {...props} addToSavedList={addToSavedList} />} />
     </div>
   );
 };
