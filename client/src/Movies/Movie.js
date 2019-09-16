bd763c4dae1730449ca48d2614b9927df372143c
ericbanker12 <ericbanker12@gmail.com>
2019-09-16 15:20:26 -0500

    add links to movies and home

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -5,7 +5,7 @@ const Movie = (props) => {
   const [movie, setMovie] = useState({});
  
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -18,7 +18,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[props]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
@@ -26,7 +26,7 @@ const Movie = (props) => {
   //   addToSavedList(movie)
   // }
 
-  if (!movie) {
+  if (!Object.keys(movie).length) {
     return <div>Loading movie information...</div>;
   }
 
